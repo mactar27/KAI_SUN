4fb6ef817c25b7e63c22c55c0e94ab3a450a5885
mactar27 <ndiayeamadoumactar3@gmail.com>
2026-07-23 18:43:44 +0000

    Add persistent shopping cart icon to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ShopContext } from './../context/ShopContext';
+import { ShoppingBag } from 'lucide-react';
 
 const Navbar = () => {
+  const { cart } = useContext(ShopContext);
+  const cartItemCount = cart.reduce((total, item) => total + item.quantity, 0);
+
   return (
     <>
       <div className="promo-bar" id="promoBar">
@@ -24,10 +29,33 @@ const Navbar = () => {
             <a href="/#collection">Collection</a>
             <a href="/#savoir-faire">Savoir-faire</a>
             <a href="/#avis">Avis</a>
-            {/* Added a link to the cart so we don't lose that functionality */}
-            <Link to="/panier">Panier</Link>
           </div>
-          <a href="/#collection" className="nav-cta">Voir la collection</a>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
+            <Link to="/panier" style={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#111' }}>
+              <ShoppingBag size={24} strokeWidth={2.5} />
+              {cartItemCount > 0 && (
+                <span style={{
+                  position: 'absolute',
+                  top: '-5px',
+                  right: '-5px',
+                  background: '#F4C430',
+                  color: '#111',
+                  fontSize: '10px',
+                  fontWeight: 900,
+                  width: '18px',
+                  height: '18px',
+                  borderRadius: '50%',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  border: '2px solid #5EB8EA'
+                }}>
+                  {cartItemCount}
+                </span>
+              )}
+            </Link>
+            <a href="/#collection" className="nav-cta">Voir la collection</a>
+          </div>
         </div>
       </nav>
     </>
