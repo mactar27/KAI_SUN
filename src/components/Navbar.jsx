f32ee8528cac8b461323ff54b8fbfd42495e0abe
mactar27 <ndiayeamadoumactar3@gmail.com>
2026-08-17 15:07:47 +0000

    feat: remplacement logo par monogramme K (kaia-logo-v3.png)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,10 +47,10 @@ const Navbar = () => {
         <div className="wrap">
           <Link to="/" className="logo">
             <img
-              src="/images/kaia_logo.png"
+              src="/kaia-logo-v3.png"
               alt="KAÏA SUNGLASSES"
               style={{
-                height: '44px',
+                height: '40px',
                 width: 'auto',
                 filter: isHomePage ? 'brightness(0) invert(1)' : 'brightness(0)',
                 transition: 'filter 0.3s'
